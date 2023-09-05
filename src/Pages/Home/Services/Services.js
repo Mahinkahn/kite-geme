8224747e4ee430db6f2710645ee5285dde043682
Mahinkahn <mahinkahn37@gmail.com>
2023-09-05 19:59:32 +0600

    se

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -5,8 +5,18 @@ const Services = () => {
 
 
     return (
-        <div className=''>
+        <div className="relative w-1/2 max-w-[300px]">
 
+            <div className="group">
+                <img
+                    src="https://www.kitegamesstudio.com/wp-content/uploads/2022/07/redoan2.png"
+                    alt="Avatar"
+                    className="block w-full h-auto"
+                />
+                <div className="absolute inset-x-0  bottom-0 flex h-2/4 items-center justify-center bg-[#4caf50] bg-opacity-50 text-white text-lg transition-opacity opacity-0 group-hover:opacity-100">
+                    My Name is John
+                </div>
+            </div>
         </div>
     );
 };
